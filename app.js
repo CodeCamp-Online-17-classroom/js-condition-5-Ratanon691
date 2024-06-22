1878be7a6cedb315476876cdb31636202f3f00b7
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-22 13:21:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// write code here
+let value = prompt(`give me a number`)
+if(value < 0) {
+  alert(`Negative`)
+}
+else if(value == 0) {
+  alert(`Zero`)
+}
+else if(value > 0) {
+  alert(`Positive`)
+}
+else if(typeof value !== `number`){
+  alert(`Invalid number`)
+}
